fa30daf473228244b27f4a8c2eed94c17a123d58
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2022-01-06 21:40:25 -0700

    Getting there

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
 window.onload = () => {
-    ;
+
+    const song_img_el = document.getElementById('song-image');
+    const song_title_el = document.getElementById('song-title');
+    const song_artist_el = document.getElementById('song-artist');
+    const song_next_up_el = document.getElementById('song-next-up');
+    
+    const play_btn = document.getElementById('play-btn');
+    const play_btn_icon = document.getElementById('play-icon');
+    const prev_btn = document.getElementById('prev-btn');
+    const next_btn = document.getElementById('next-btn');
+
+    const audio_player = decoument.getElementById('music-player');
+
+    let current_song_index;
+    let next_song_index;
+
+    let songs = [
+
+        {
+            title: 'Faded',
+            artist: 'Alan Walker',
+            song_path: 'music/Faded.mp3',
+            img_path: 'images/faded.jpg'
+        },
+
+        {
+            title: '',
+            artist: '',
+            song_path: '',
+            img_path: ''
+        },
+
+        {
+            title: '',
+            artist: '',
+            song_path: '',
+            img_path: ''  
+        },
+
+        {
+            title: '',
+            artist: '',
+            song_path: '',
+            img_path: ''
+        }
+        
+    ]
+
+    InitPlayer();
+
+    function InitPlayer() {
+
+    }
+
 }
