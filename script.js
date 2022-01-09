3a20e3adb3c7ee74550b4cdceda31a9bea4e21ea
Traian Alex Oglinzanu <oglinzan@ualberta.ca>
2022-01-08 21:08:40 -0700

    All done button functionality working

--- script.js
+++ script.js
@@ -47,6 +47,11 @@ window.onload = () => {
 
     ]
 
+    play_btn.addEventListener('click', TogglePlaySong);
+    next_btn.addEventListener('click', () => ChangeSong());
+    prev_btn.addEventListener('click', () => ChangeSong(false));
+
+
     InitPlayer();
 
     function InitPlayer() {
@@ -55,19 +60,63 @@ window.onload = () => {
         UpdatePlayer();
     }
 
-    function UpdatePlayer {
+    function UpdatePlayer() {
 
         let song = songs[current_song_index];
 
         song_img_el.style = "background-image: url('" + song.img_path + "')";
-
         song_title_el.innerText = song.title;
         song_artist_el.innerText = song.artist;
 
-        song_next_up_el.innerText = songs[next_song_index].title + "by" + songs[next_song_index].artist;
+        song_next_up_el.innerText = songs[next_song_index].title + " by " + songs[next_song_index].artist;
 
         audio_player.src = song.song_path;
 
     }
 
+    function TogglePlaySong() {
+
+        if (audio_player.paused){
+
+            audio_player.play();
+            play_btn_icon.classList.remove('fa-play');
+            play_btn_icon.classList.add('fa-pause');
+
+        } else {
+
+            audio_player.pause();
+            play_btn_icon.classList.add('fa-play');
+            play_btn_icon.classList.remove('fa-pause');
+
+        }
+    }
+
+    function ChangeSong (next = true) {
+        if (next) { 
+            current_song_index++;
+            next_song_index = current_song_index + 1;
+
+            if(current_song_index > songs.length - 1){
+                current_song_index = 0;
+                next_song_index = current_song_index + 1;
+            }
+
+            if(next_song_index > songs.length - 1){
+                next_song_index = 0;
+            }
+
+        }else {
+            current_song_index--;
+            next_song_index = current_song_index + 1;
+
+            if(current_song_index < 0){
+                current_song_index = songs.length - 1;
+                next_song_index = 0;
+            }
+        }
+
+        UpdatePlayer();
+        TogglePlaySong();
+
+    }
 }
